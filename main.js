ce7625e3b158bb8b9186789126d49cb9b5b34aaf
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-24 18:05:01 +0200

    [UPD] variables for under 18 and over 65

--- main.js
+++ main.js
@@ -7,25 +7,35 @@
 //età
 const age = prompt('inserisci la tua età') ;
 const userAge= Number(age) ;
-console.log(`questa è l'età dell'utente ${userAge}`);
+console.log(`l'utente ha ${userAge} anni`);
 
 //km - distance
 const km = prompt('inserisci i kilometri da percorrere');
 const distance =Number(km) ;
-console.log(`questa è la distanza ${distance} km`);
+console.log(`distanza che deve percorrere ${distance} km`);
 
 //costo iniziale del biglietto
 const ticket = 0.21
-console.log(`questo è il coso iniziale del biglietto ${ticket}`);
+console.log(`costo iniziale del biglietto ${ticket}`);
 
 //in base  alle informazioni fornite calcola il prezzo, 0,21euro al km.
-console.log(`questo è il prezzo del ticket in base alla distanza percorsa`+ (ticket * distance));
+const ticketCost =  ticket * distance ;
+console.log(`costo del biglietto: ${ticketCost}€ `);
 
-//costo del ticket al kilometro
-const ticketCost =  ticket * distance;
-console.log(`costo del ticket  al km ${ticketCost}`);
 
 // se sotto i 18 anni sconto del 20%
+const under18 = 20/100;
+const u18TicketCost = ticketCost - under18 +"€";
+
+//se  oltre ai 65 anni sconti del 40%
+const over65 = 40/100;
+const o65TicketCost  = ticketCost - over65 +"€";
+
+
+//debug
+console.log(`sconto applicato ${u18TicketCost}`);
+
+
 // se l'età è compresa tra 18 e 65 calcolo normale
 // se sopra i 65 va applicato uno sconto del 40%
 //l'output del prezzo è rielaborato con massimo 2 decimali.
