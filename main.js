29986e87de41fcb784e881b1a4f443279a4a55e8
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-24 17:05:48 +0200

    [UPD] variables cost of the initial ticket, cost of the ticket per km

--- main.js
+++ main.js
@@ -4,24 +4,27 @@
 //il programma richiede un input all'utente tramite prompt:
 //-età, -numero km.--> queste 2 informazioni vanno usate come variabili.
 
-
+//età
 const age = prompt('inserisci la tua età') ;
-//eseguo il debug per comprendere se il parametro è stato passato correttamente
-console.log(`age is ${age}`);
-//identifico la variabile come stringa
-console.log(typeof age);
-//definisco la mia variabile da stringa a numero intero
 const userAge= Number(age) ;
-//identifico la variabie come num.int
-console.log(typeof userAge);
+console.log(`questa è l'età dell'utente ${userAge}`);
 
+//km - distance
 const km = prompt('inserisci i kilometri da percorrere');
-console.log(`km is ${km}`) ;
-console.log(typeof km) ;
-let distance =Number(km) ;
-console.log(typeof distance) ;
+const distance =Number(km) ;
+console.log(`questa è la distanza ${distance} km`);
+
+//costo iniziale del biglietto
+const ticket = 0.21
+console.log(`questo è il coso iniziale del biglietto ${ticket}`);
 
 //in base  alle informazioni fornite calcola il prezzo, 0,21euro al km.
+console.log(`questo è il prezzo del ticket in base alla distanza percorsa`+ (ticket * distance));
+
+//costo del ticket al kilometro
+const ticketCost =  ticket * distance;
+console.log(`costo del ticket  al km ${ticketCost}`);
+
 // se sotto i 18 anni sconto del 20%
 // se l'età è compresa tra 18 e 65 calcolo normale
 // se sopra i 65 va applicato uno sconto del 40%
