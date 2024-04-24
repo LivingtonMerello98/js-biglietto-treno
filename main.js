03199ea4dd4bb5ef3f88f3a1fb33e30ef98566ad
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-24 15:44:12 +0200

    [UPD] steps to follow to build the program

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+//il programma richiede un input all'utente tramite prompt:
+//-età, -numero km.--> queste 2 informazioni vanno usate come variabili.
+//in base  alle informazioni fornite calcola il prezzo, 0,21euro al km.
+// se sotto i 18 anni sconto del 20%
+// se l'età è compresa tra 18 e 65 calcolo normale
+// se sopra i 65 va applicato uno sconto del 40%
+//l'output del prezzo è rielaborato con massimo 2 decimali.
+
