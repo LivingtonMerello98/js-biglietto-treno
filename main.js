d74d452d328505e1c5fc40dbb444c2fc2a541bb5
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-24 15:49:51 +0200

    [UPD] debug "ciao mondo"

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+console.log("ciao mondo")
+
 //il programma richiede un input all'utente tramite prompt:
 //-età, -numero km.--> queste 2 informazioni vanno usate come variabili.
 //in base  alle informazioni fornite calcola il prezzo, 0,21euro al km.
@@ -7,4 +9,5 @@
 // se l'età è compresa tra 18 e 65 calcolo normale
 // se sopra i 65 va applicato uno sconto del 40%
 //l'output del prezzo è rielaborato con massimo 2 decimali.
+
 
