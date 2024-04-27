a24856a4cecb0530ad5078bd1afffd3dc2ef3271
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-27 18:58:05 +0200

    [upd] we assign a value to "result" through a conditional

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,11 +1,11 @@
 
 //chiedere all'utente se vuole pari o dispari
-const userChoice= prompt("scegli pari o dispari");
-console.log("l'utente ha deciso"+" "+userChoice);
+const userChoice = prompt("scegli pari o dispari");
+console.log("hai deciso ha deciso"+" "+userChoice);
  
 //chiedere all'utente un numero compreso tra 1 e 9 
-const numberUser = prompt("scegli un numero");
-console.log("l'utente ha deciso"+" "+numberUser);
+const numberUser = Number(prompt("Scegli un numero tra 1 e 9:"));
+console.log("hai deciso"+" "+numberUser);
  
 //generare un numero tra 1 e 9 che sarà giocato dal pc 
 let numberComputer = Math.floor(Math.random() * 9) + 1;
@@ -13,7 +13,15 @@ console.log("il computer ha generato"+" "+numberComputer);
  
 //si somma il numero scelto dall'utente e il numero del pc
 const sum = numberUser + numberComputer;
+console.log("la somma tra la tua scelta e quella  del computer è di"+" "+sum);
  
 //si crea una variabile let da riutilizzare all'interno dei condizionali
 let result = 0;
-  
+
+//si determina se la somma è pari o dispari
+if (sum % 2 === 0){
+    result ="pari";
+} else {
+    result ="dispari";
+};
+
