87f476eb3695d34994076993cdbcf7b86dbb45aa
LivingtonMerello98 <livington.merello@gmail.com>
2024-04-27 18:35:19 +0200

    [upd] determine whether the sum is even or odd

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,12 +1,15 @@
 
 //chiedere all'utente se vuole pari o dispari
 const userChoice= prompt("scegli pari o dispari");
+console.log("l'utente ha deciso"+" "+userChoice);
  
 //chiedere all'utente un numero compreso tra 1 e 9 
 const numberUser = prompt("scegli un numero");
+console.log("l'utente ha deciso"+" "+numberUser);
  
 //generare un numero tra 1 e 9 che sarà giocato dal pc 
 let numberComputer = Math.floor(Math.random() * 9) + 1;
+console.log("il computer ha generato"+" "+numberComputer);
  
 //si somma il numero scelto dall'utente e il numero del pc
 const sum = numberUser + numberComputer;
